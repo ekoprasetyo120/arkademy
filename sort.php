1afd9fca4f6839240caf5674568b011d877ef0a4
Kii <32608249+kidszonk@users.noreply.github.com>
2019-06-29 22:28:01 +0700

    Create sort.php

---
+++ sort.php
@@ -0,0 +1,22 @@
+<?php
+  //input
+   $random = 5956560159466056;
+   $data = explode(0, $random);
+   echo $data[0]."\n";
+   echo $data[1]."\n";
+   echo $data[2]."<br>";
+ 
+ //output
+ echo $data[0][2];
+ echo $data[0][4];
+ echo $data[0][3];
+ echo $data[0][5];
+ echo $data[0][1];
+ echo $data[1][0];
+ echo $data[1][3];
+ echo $data[1][1];
+ echo $data[1][4];
+ echo $data[1][5];
+ echo $data[1][2];
+ echo $data[2];
+?>
